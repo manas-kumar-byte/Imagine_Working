253b5ad1fa154866f9d4b89f1840bdcdf6640876
Ritwik Yadav <ritwikyadav35@gmail.com>
2026-09-13 06:17:10 +0530

    whoops

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
-import {getFacilities} from '/home/ritwikyadav/Imagine_Working/frontend/src/api/client.js';
+//import {getFacilities} from '..';
 
-const facilities = await(getFacilities());
+// const facilities = await(getFacilities());
 console.log(facilities)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
